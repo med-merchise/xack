4e8672ffc9c2edb36a6c574f7a4dff4544a91516
Medardo Antonio Rodriguez <med.merchise@gmail.com>
2024-11-08 07:08:46 -0500

    Add module for data structures

---
+++ src/xack/ds/__init__.py
@@ -0,0 +1,7 @@
+"""Data structures.
+
+Data structures define how to organize, store, and manipulate data.  Due to
+the purpose of this application, the same concept could have several
+implementations for testing in different contexts.
+
+"""
